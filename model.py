8f0bf8c039494b89d4318b700f88593333d82491
Library User <libraryuser@dhcp-gs-7794.redrover.cornell.edu>
2020-02-29 21:58:49 -0500

    Work on the Machine Learning Model and update Details

--- model.py
+++ model.py
@@ -2,30 +2,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
-
-dataset = pd.read_csv('sales.csv')
-
-dataset['rate'].fillna(0, inplace=True)
-
-dataset['sales_in_first_month'].fillna(dataset['sales_in_first_month'].mean(), inplace=True)
-
-X = dataset.iloc[:, :3]
-
-def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
-    return word_dict[word]
-
-X['rate'] = X['rate'].apply(lambda x : convert_to_int(x))
-
-y = dataset.iloc[:, -1]
-
 from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+from sklearn import metrics
+# from sklearn.cross_validation import train_test_split
 
-regressor.fit(X, y)
 
-pickle.dump(regressor, open('model.pkl','wb'))
+#please write comments
+def categorical_to_continuous(df):
+    lst = []
+    for i in df.columns:
+        if df[i].dtype == "object":
+            if len(df[i].unique())>5:
+                lst.append(i)
+            else:
+                df[i] = df[i].astype("category")
+                df[i] = df[i].cat.codes
+        else:
+            if 1.*df[i].nunique()/df[i].count() > 0.50: #i might need to change the threshold
+                lst.append(i)
 
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[4, 300, 500]]))
+    return lst
+
+
+#make this function more efficient
+def fill_nan_vals (df):
+    for i in df.columns:
+        df[i] = df[i].replace('',np.nan, inplace = True)
+
+    for i in df.columns:
+        df[i] = df[i].replace(np.inf,np.nan, inplace = True)
+    
+    # for i in df.columns:
+    #     df[i] = df[i].fillna(0, inplace = True)
+
+    return df.fillna(0)
+
+dataset = pd.read_csv('train.csv')
+pred_var = "Survived" #this needs to have its own inputs
+
+Y = dataset[pred_var] #target variable
+
+dataset = dataset.drop([pred_var], axis = 1)
+
+drop_lst = categorical_to_continuous(dataset)
+
+dataset = dataset.drop(drop_lst, axis = 1)
+# dataset = dataset.repalce('', np.nan, inplace = True)
+dataset = fill_nan_vals(dataset)
+
+X = dataset
+
+regressor = LinearRegression().fit(X,Y)
+
+print (regressor.score(X,Y))
+
+# pickle.dump(regressor, open('model.pkl','wb'))
+
+# model = pickle.load(open('model.pkl','rb'))
